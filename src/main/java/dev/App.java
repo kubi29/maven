878826efdb4fdf97b736c34306353e47ad6f8e93
Alex <a.peran@orange.fr>
2017-09-28 15:09:10 +0200

    4e commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,17 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine( "Hello World!");
+        System.out.println( asciiArt );
     }
 }
